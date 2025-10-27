ec7736a8c50d62aaffd19e9955d91a1299c72f55
omarelkadiri <omarelkadiri2035@gmail.com>
2025-10-27 17:57:25 +0100

    Configuration d'un gateway dynamique

--- gateway/src/main/java/ccn/elkadiri/gateway/GatewayApplication.java
+++ gateway/src/main/java/ccn/elkadiri/gateway/GatewayApplication.java
@@ -2,7 +2,10 @@ package ccn.elkadiri.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 @SpringBootApplication
 public class GatewayApplication {
 
@@ -10,4 +13,10 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+    @Bean
+    DiscoveryClientRouteDefinitionLocator locator(
+            ReactiveDiscoveryClient rdc,
+            DiscoveryLocatorProperties dlp) {
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
+    }
 }
